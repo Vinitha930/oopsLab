205568a633c903d9320157bd1199595b486873af
Vinitha930 <96111104+Vinitha930@users.noreply.github.com>
2022-03-21 15:18:22 +0530

    display user defined array

---
+++ Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Array
+ {
+    public static void main(String[] args)
+      {
+        int a[]=new int [5];
+         int i;
+ System.out.println("enter 5 numbers");
+Scanner s1=new Scanner(System.in);
+ for(i=0;i<5;i++)
+   {
+    a[i]=s1.nextInt();
+  }
+ System.out.println("the array is"); 
+ 
+for(i=0;i<5;i++)
+   {
+    System.out.print("\t"+a[i]); 
+ 
+ 
+}} }
