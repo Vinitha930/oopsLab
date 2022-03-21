089bedcbb06e5fbdb632af0cfe26ba6dbb909127
Vinitha930 <96111104+Vinitha930@users.noreply.github.com>
2022-03-21 15:16:56 +0530

    display employee details

---
+++ Employee.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Employee
+  {
+   public static void main(String args[])
+     {
+         String name;
+int age;
+float salary;
+Scanner s1=new Scanner(System.in);
+System.out.println("enter name,age,salary");
+name=s1.nextLine();
+age=s1.nextInt();
+salary=s1.nextInt();
+System.out.println("name is "+ name +"\nage is "+ age +" \n salary is "+ salary);
+}                                                                                                                                                                 
+}
+
+
+
+
