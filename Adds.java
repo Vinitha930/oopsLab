cb3ace5a1f00469d69d9d602f82f86daa95f3e72
Vinitha930 <96111104+Vinitha930@users.noreply.github.com>
2022-03-21 15:16:21 +0530

    add two user defined numbers

---
+++ Adds.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Adds
+  {
+    public static void main(String[] args)
+      {
+         int a,b,c;
+      Scanner s1=new Scanner(System.in);
+System.out.println("enter 2 nos");
+a=s1.nextInt();
+b=s1.nextInt();
+c=a+b;
+System.out.println("sum is "+c);
+}
+}
