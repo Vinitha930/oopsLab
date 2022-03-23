49ce86dde58d08fc54d47f09b285f8bb799b1e58
Vinitha930 <96111104+Vinitha930@users.noreply.github.com>
2022-03-23 11:48:40 +0530

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -5,17 +5,17 @@ class Complex
     {
       int a,b,c,d,e,f;
       Scanner s1=new Scanner(System.in);
- System.out.println("enter real part of first number");
-  a=s1.nextInt();
- System.out.println("enter img part of first number");
-  b=s1.nextInt();
- System.out.println("enter real part of second number");
-  c=s1.nextInt();
- System.out.println("enter img part of second number");
-  d=s1.nextInt();
- System.out.println("the sum is");
-  e=a+c;
-  f=b+d;
-System.out.println(e+"+"+f+"i");
-}
+      System.out.println("enter real part of first number");
+      a=s1.nextInt();
+      System.out.println("enter imaginary part of first number");
+      b=s1.nextInt();
+      System.out.println("enter real part of second number");
+      c=s1.nextInt();
+      System.out.println("enter img part of second number");
+      d=s1.nextInt();
+      System.out.println("the sum is");
+      e=a+c;
+      f=b+d;
+     System.out.println(e+"+"+f+"i");
+    }
 }
